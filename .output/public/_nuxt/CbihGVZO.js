f946b64967936908f12747e076c5faff0308b010
Ashimriat <ashimriat42@gmail.com>
2025-01-09 21:34:42 +0300

    тест 12

--- .output/public/_nuxt/CbihGVZO.js
+++
@@ -1,1 +0,0 @@
-import{b1 as r}from"./CNUPeZ-s.js";var e=r();export{e as O};
--- .output/public/_nuxt/DLIpHRn9.js
+++
@@ -1,1 +0,0 @@
-import{d as a,a as s,c as n,n as o,e as r,o as t}from"./CNUPeZ-s.js";const p=a({__name:"Gallery",setup(c){const e=s();return(l,m)=>(t(),n("div",{class:o(r(e)())},"Галлерея",2))}});export{p as default};
