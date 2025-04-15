d10329896ebb3549ead5d81f15571a6b0bd0514c
Iaroslav Aksenov <ashimriat42@gmail.com>
2025-04-16 02:17:23 +0300

    Фикс ТМА

--- plugins/telegram.ts
+++ plugins/telegram.ts
@@ -11,39 +11,38 @@ import {
 } from '@telegram-apps/sdk-vue'
 
 export default defineNuxtPlugin(() => {
-  const { isMobile } = useDevice();
+  const { isDesktop } = useDevice();
   const route = useRoute()
   const router = useRouter()
 
   const backButtonClickHandler = () => router.back();
 
-  const isMiniApp = isTMA()
 
   if (mountBackButton.isAvailable()) {
     mountBackButton()
   }
 
+  const isMiniApp = isTMA();
+
   onNuxtReady(() => {
     if (!isMiniApp) return;
-    init();
-    if (!isMobile) {
-      expandViewport();
-    }
-
-    watch(
-      () => route.path,
-      () => {
-        if (!isMiniApp) return;
-        if (route.path === '/') {
-          hideBackButton()
-          offBackButtonClick(backButtonClickHandler);
-          return;
-        }
-        if (isBackButtonVisible()) return;
-        showBackButton()
-        onBackButtonClick(backButtonClickHandler)
-      },
-      { immediate: true },
-    )
+    init({ acceptCustomStyles: false });
+    if (isDesktop) expandViewport();
   })
+
+  watch(
+    () => route.path,
+    () => {
+      if (!isMiniApp) return;
+      if (route.path === '/') {
+        hideBackButton()
+        offBackButtonClick(backButtonClickHandler);
+        return;
+      }
+      if (isBackButtonVisible()) return;
+      showBackButton()
+      onBackButtonClick(backButtonClickHandler)
+    },
+    { immediate: true },
+  )
 })
